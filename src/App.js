7ac8fc29c0ba93c570e89622653d6e5c3b4b58c0
Rathapong <dev01tcm@outlook.com>
2021-10-08 15:57:43 +0700

    defaul

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Dashboard from "./component/Dashboard"
+import Footer from "./menu/Footer"
+import Navbar from "./menu/Navbar"
+import Sidebar from "./menu/Sidebar"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<div>
+				<Sidebar />
+
+				<Navbar />
+				<Dashboard />
+				<Footer />
+			</div>
+		</>
+	)
 }
 
-export default App;
+export default App
