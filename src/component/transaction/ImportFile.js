bcab71a2378f523f417707aff167257e0d2dbf3b
ppoptimus <r.pumpo@gmail.com>
2021-11-04 21:46:19 +0700

    start read text file

--- src/component/transaction/ImportFile.js
+++ src/component/transaction/ImportFile.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import XLSX from 'xlsx'
+import Swal from "sweetalert2"
 
 export default function ImportFile() {
 	let [selectedFile, setSelectedFile] = useState([])
-	const onReadFile = (file) => {
+
+	const onReadExcelFile = (file) => {
 		const promise = new Promise((resolve, reject) => {
 			const fileReader = new FileReader()
 			fileReader.readAsArrayBuffer(file)
@@ -23,13 +25,49 @@ export default function ImportFile() {
 			}
 		})
 		promise.then((data) => {
+			Swal.fire({
+				title: "สร้างเลขชุดหนังสือสำเร็จ",
+				icon: "success",
+				confirmButtonColor: "#119516",
+				confirmButtonText: "ตกลง",
+			}).then((result) => {
+				if (result.isConfirmed) {
+					
+				}
+			})
 			console.log(data)
 			setSelectedFile(data)
 		})
 	}
+
+	const onReadTextFile = (e) => {
+		const reader = new FileReader()
+		reader.onload = (e) => {
+			const text = e.target.result
+			const arr = text.toString().replace(/\r\n/g,'\n').split('\n');
+			for(let row of arr) {
+				console.log(row);
+			}
+
+		}
+		reader.readAsText(e.target.files[0])
+	}
 	return (
 		<>
-			<input type='file' onChange={(e) => onReadFile(e.target.files[0])}></input>
+			<div className='row flex justify-content-around'>
+				<div className='custom-file col-5'>
+					<input type='file' className='custom-file-input' id='exampleInputFile' onChange={(e) => onReadExcelFile(e.target.files[0])} />
+					<label className='custom-file-label' htmlFor='exampleInputFile'>
+						อัพโหลด Excel
+					</label>
+				</div>
+				<div className='custom-file col-5'>
+					<input type='file' className='custom-file-input' id='exampleInputFile' onChange={(e) => onReadTextFile(e)} />
+					<label className='custom-file-label' htmlFor='exampleInputFile'>
+						อัพโหลด Text File
+					</label>
+				</div>
+			</div>
 		</>
 	)
 }
