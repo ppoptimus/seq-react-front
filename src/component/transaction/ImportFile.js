b35fccc11b2b5fcf37d74f1f37a820302e46d6b0
Rathapong <dev01tcm@outlook.com>
2021-11-19 16:30:15 +0700

    start digit

--- src/component/transaction/ImportFile.js
+++ src/component/transaction/ImportFile.js
@@ -11,6 +11,18 @@ export default function ImportFile() {
 	let [importObject, setimportObject] = useState([])
 	let [dataset] = useState([])
 
+	const onSelectFile = (file) => {
+		if (file.target.files[0].type === "text/plain" || file.target.files[0].type === "text/csv") {
+			ReadTextFile(file)
+		} else if (
+			file.target.files[0].type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
+			file.target.files[0].type === "application/vnd.ms-excel"
+		) {
+			onReadExcelFile(file.target.files[0])
+		} else {
+			console.log("err")
+		}
+	}
 	const onReadExcelFile = (file) => {
 		const promise = new Promise((resolve, reject) => {
 			const fileReader = new FileReader()
@@ -18,69 +30,70 @@ export default function ImportFile() {
 
 			fileReader.onload = (e) => {
 				const bufferArray = e.target.result
-
 				const wb = XLSX.read(bufferArray, { type: "binary" })
 				const wsname = wb.SheetNames[0]
 				const ws = wb.Sheets[wsname]
-				
-				if (ws.A1.w.length > 5) {
-					let range = XLSX.utils.decode_range(ws["!ref"])
-					range.s.r = 1
-					ws["!ref"] = XLSX.utils.encode_range(range)
-				}
-				const data = XLSX.utils.sheet_to_json(ws, {
-					header: [
-						"request_code",
-						"document_set_no",
-						"document_date",
-						"employer_account",
-						"title_name",
-						"first_name",
-						"last_name",
-						"refference_id",
-						"bank_code",
-						"branch_code",
-						"status_code",
-						"branch_name",
-						"account_type_code",
-						"account_no",
-						"account_name",
-						"rating",
-						"balance",
-						"investigate_date",
-						"remark",
-					],
-				})
+				try {
+					if (ws.A1.w.length > 5) {
+						let range = XLSX.utils.decode_range(ws["!ref"])
+						range.s.r = 1
+						ws["!ref"] = XLSX.utils.encode_range(range)
+					}
 
-				setimportObject({
-					request_code: ws.A2.w,
-					document_set: ws.B2.w.trim(),
-					bank_code: ws.I2.w,
-					user_name: userDetail.username,
-				})
-
-				for (const item of data) {
-					
-					dataset.push({
-						document_date: item.document_date,
-						employer_account: item.employer_account.toString(),
-						title_name: item.title_name ? item.title_name.trim() : null,
-						first_name: item.first_name ? item.first_name.trim() : null,
-						last_name: item.last_name ? item.last_name.trim() : null,
-						refference_id: (item.refference_id === undefined) ? null : item.refference_id.toString().trim(),
-						branch_code: item.branch_code ? item.branch_code : null,
-						status_code: item.status_code,
-						branch_name: (item.branch_name === undefined) ? null : item.branch_name.toString().trim(),
-						account_type_code: (item.account_type_code === undefined) ? null : item.account_type_code,
-						account_no: (item.account_no === undefined) ? null : item.account_no.toString().trim(),
-						account_name: (item.account_name === undefined) ? null : item.account_name.toString().trim(),
-						balance: (item.balance === undefined) ? null : item.balance,
-						investigate_date: ws.R2.w,
-						remark: (item.remark === undefined) ? null : item.remark.toString().trim(),
+					const data = XLSX.utils.sheet_to_json(ws, {
+						header: [
+							"request_code",
+							"document_set_no",
+							"document_date",
+							"employer_account",
+							"title_name",
+							"first_name",
+							"last_name",
+							"refference_id",
+							"bank_code",
+							"branch_code",
+							"status_code",
+							"branch_name",
+							"account_type_code",
+							"account_no",
+							"account_name",
+							"rating",
+							"balance",
+							"investigate_date",
+							"remark",
+						],
 					})
+
+					setimportObject({
+						request_code: ws.A2.w,
+						document_set: ws.B2.w.trim(),
+						bank_code: ws.I2.w,
+						user_name: userDetail.username,
+					})
+
+					for (const item of data) {
+						dataset.push({
+							document_date: item.document_date,
+							employer_account: item.employer_account.toString(),
+							title_name: item.title_name ? item.title_name.trim() : null,
+							first_name: item.first_name ? item.first_name.trim() : null,
+							last_name: item.last_name ? item.last_name.trim() : null,
+							refference_id: item.refference_id === undefined ? null : item.refference_id.toString().trim(),
+							branch_code: item.branch_code ? item.branch_code : null,
+							status_code: item.status_code,
+							branch_name: item.branch_name === undefined ? null : item.branch_name.toString().trim(),
+							account_type_code: item.account_type_code === undefined ? null : item.account_type_code,
+							account_no: item.account_no === undefined ? null : item.account_no.toString().trim(),
+							account_name: item.account_name === undefined ? null : item.account_name.toString().trim(),
+							balance: item.balance === undefined ? null : item.balance,
+							investigate_date: ws.R2.w,
+							remark: item.remark === undefined ? null : item.remark.toString().trim(),
+						})
+					}
+					resolve(data)
+				} catch (err) {
+					console.log(err)
 				}
-				resolve(data)
-				// console.log(data)
 			}
 			fileReader.onerror = (err) => {
 				reject(err)
@@ -89,48 +102,38 @@ export default function ImportFile() {
 		promise.then(() => {})
 	}
 
-	const onInputTextFile = (e) => {
-		try {
-			const ext = e.target.files[0].type
-			if (!!ext && (ext === "text/plain" || ext === "text/csv")) {
-				ReadTextFile(e)
-			} else {
-				refreshPage()
-			}
-		} catch {
-			refreshPage()
-		}
-	}
-
 	const ReadTextFile = (e) => {
 		const reader = new FileReader()
 		reader.onload = (e) => {
 			const text = e.target.result
 			try {
 				const arr = text.toString().replace(/\r\n/g, "\n").split("\n")
+				// console.log(["1", "2", "3", "4"].includes(arr[0].substr(192, 1)))
+
 				setimportObject({
 					request_code: arr[0].substr(0, 5),
 					document_set: arr[0].substr(5, 20).trim(),
 					bank_code: arr[0].substr(185, 3),
 					user_name: userDetail.username,
 				})
-
+			console.log(arr[0].substr(185, 3))
 				for (let i = 0; i < arr.length - 1; i++) {
 					let document_date = arr[i].substr(25, 10)
 					let employer_account = arr[i].substr(35, 10)
-					let title_name = arr[i].substr(45, 20)
-					let first_name = arr[i].substr(65, 50)
-					let last_name = arr[i].substr(115, 50)
+					let title_name = arr[i].substr(45, 20).trim()
+					let first_name = arr[i].substr(65, 50).trim()
+					let last_name = arr[i].substr(115, 50).trim()
 					let refference_id = arr[i].substr(165, 20)
-					let branch_code = arr[i].substr(188, 4)
-					let status_code = arr[i].substr(192, 1)
-					let branch_name = arr[i].substr(193, 50)
-					let account_type_code = arr[i].substr(243, 3)
-					let account_no = arr[i].substr(245, 20)
-					let account_name = arr[i].substr(265, 100)
-					let balance = arr[i].substr(366, 17)
-					let investigate_date = arr[i].substr(383, 30)
-					let remark = arr[i].substr(413, 50)
+
+					let branch_code = (!arr[0].substr(185, 3)==='006') ? arr[i].substr(188, 4) : arr[i].substr(188, 6).trim()
+					let status_code = (!arr[0].substr(185, 3)==='006') ? arr[i].substr(192, 1) : arr[i].substr(194, 1)
+					let branch_name = (!arr[0].substr(185, 3)==='006') ? arr[i].substr(193, 50) : arr[i].substr(195, 50).trim()
+					let account_type_code = (!arr[0].substr(185, 3)==='006') ? arr[i].substr(243, 2) : arr[i].substr(245, 2).trim()
+					let account_no = (!arr[0].substr(185, 3)==='006') ? arr[i].substr(245, 20) : arr[i].substr(247, 20).trim()
+					let account_name = (!arr[0].substr(185, 3)==='006') ? arr[i].substr(265, 100) : arr[i].substr(267, 100).trim()
+					let balance = (!arr[0].substr(185, 3)==='006') ? arr[i].substr(366, 17) : arr[i].substr(368, 17).trim()
+					let investigate_date = (!arr[0].substr(185, 3)==='006') ? arr[i].substr(382, 30) : arr[i].substr(384, 30).trim()
+					let remark = (!arr[0].substr(185, 3)==='006') ? arr[i].substr(413, 50) : arr[i].substr(415, 50).trim()
 
 					dataset.push({
 						document_date: document_date,
@@ -169,6 +172,7 @@ export default function ImportFile() {
 	}
 
 	const onUpload = () => {
+		console.log(dataset)
 		const result = { dataset, ...importObject }
 		const config = {
 			method: "post",
@@ -197,11 +201,11 @@ export default function ImportFile() {
 						type='file'
 						className='custom-file-input'
 						id='exampleInputFile'
-						accept='.csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel'
-						onChange={(e) => onReadExcelFile(e.target.files[0])}
+						accept='.csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel, .txt, text/plain'
+						onChange={(e) => onSelectFile(e)}
 					/>
 					<label className='custom-file-label' htmlFor='exampleInputFile'>
-						อัพโหลด Excel
+						ไฟล์ข้อมูลจากธนาคาร
 					</label>
 				</div>
 				<div className='custom-file col-5'>
@@ -210,10 +214,10 @@ export default function ImportFile() {
 						className='custom-file-input'
 						id='exampleInputFile'
 						accept='.csv, .txt, text/plain'
-						onChange={(e) => onInputTextFile(e)}
+						onChange={(e) => onSelectFile(e)}
 					/>
 					<label className='custom-file-label' htmlFor='exampleInputFile'>
-						อัพโหลด Text File
+						อัพโหลดไฟล์แนบ
 					</label>
 				</div>
 			</div>
